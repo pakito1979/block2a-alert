8495df772a44bee8befed195f1dc27a3aff8af98
pakito1979 <fcoaldea@gmail.com>
2025-12-26 00:13:56 +0100

    Arreglar indentación columnas

--- app.py
+++ app.py
@@ -127,6 +127,19 @@ with tab1:
 with tab2:
     st.subheader("Buscar noticias y analizar")
     st.write("Primero recoge noticias. Luego pulsa analizar para que aparezcan cronograma e importancia.")
+with tab2:
+    st.subheader("Buscar noticias y analizar")
+    st.write("Primero recoge noticias. Luego pulsa analizar para que aparezcan cronograma e importancia.")
+
+    col1, col2 = st.columns([1, 1])
+
+    with col1:
+        st.write("Columna izquierda")
+
+    with col2:
+        st.write("Columna derecha")
+
+
 import pandas as pd
 import plotly.express as px
 
